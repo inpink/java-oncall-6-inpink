f5c56a70a859655ab28a8e65f4dd8f85a5bc84b6
inpink <inpink@kakao.com>
2023-12-16 14:56:18 +0900

    feat: 닉네임  같은 것을 2개 이상 입력할 시 잘못된 입력

--- src/main/java/oncall/mapper/EmployeesMapper.java
+++ src/main/java/oncall/mapper/EmployeesMapper.java
@@ -1,14 +1,17 @@
 package oncall.mapper;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import oncall.domain.Employees;
+import oncall.util.ExceptionUtil;
 import oncall.util.StringUtil;
 
 public class EmployeesMapper {
 
     public static Employees toEmployees(String input) {
         List<String> separated = seperate(input);
+        validateDuplicated(separated);
 
         return Employees.create(separated);
     }
@@ -18,5 +21,14 @@ public class EmployeesMapper {
         List<String> separated = Arrays.stream(deleteSpaces.split(","))
                 .toList();
         return separated;
+    }
+
+    private static void validateDuplicated(List<String> separated) {
+        int size = separated.size();
+        int deduplicatedSize = new HashSet<>(separated).size();
+
+        if (size != deduplicatedSize) {
+            ExceptionUtil.throwInvalidValueException();
+        }
     }
 }
--- src/main/java/oncall/view/InputView.java
+++ src/main/java/oncall/view/InputView.java
@@ -13,21 +13,21 @@ import oncall.util.InputUtil;
 
 public class InputView {
     public StartDate inputStartDate() {
-        System.out.println(INPUT_ONCALL_MONTH_AND_DAY.getMessage());
+        System.out.print(INPUT_ONCALL_MONTH_AND_DAY.getMessage());
         String input = InputUtil.input();
 
         return StartDateMapper.toStartDate(input);
     }
 
     public Employees inputWorkdayEmployees() {
-        System.out.println(WORKDAY.getMessage() + INPUT_ONCALL_NICKNAMES.getMessage());
+        System.out.print(WORKDAY.getMessage() + INPUT_ONCALL_NICKNAMES.getMessage());
         String input = InputUtil.input();
 
         return EmployeesMapper.toEmployees(input);
     }
 
     public Employees inputHolidayEmployees() {
-        System.out.println(HOLIDAY.getMessage() + INPUT_ONCALL_NICKNAMES.getMessage());
+        System.out.print(HOLIDAY.getMessage() + INPUT_ONCALL_NICKNAMES.getMessage());
         String input = InputUtil.input();
 
         return EmployeesMapper.toEmployees(input);
