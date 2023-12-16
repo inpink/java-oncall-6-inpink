f1acaee3262c99e5d927e6d8b8ca350cee6294a0
inpink <inpink@kakao.com>
2023-12-16 14:20:46 +0900

    feat: 요일은 월,화,수,목,금,토,일

---
+++ src/main/java/oncall/domain/DayOfWeek.java
@@ -0,0 +1,32 @@
+package oncall.domain;
+
+import java.util.Arrays;
+import oncall.util.ExceptionUtil;
+
+public enum DayOfWeek {
+    SUNDAY("일"),
+    MONDAY("월"),
+    TUESDAY("화"),
+    WEDNESDAY("수"),
+    THURSDAY("목"),
+    FRIDAY("금"),
+    SATURDAY("토");
+
+    private final String koreanLabel;
+
+    DayOfWeek(String koreanLabel) {
+        this.koreanLabel = koreanLabel;
+    }
+
+    public static DayOfWeek find(String dayLabel) {
+        return Arrays.stream(DayOfWeek.values())
+                .filter(day -> day.koreanLabel.equals(dayLabel))
+                .findAny()
+                .orElseThrow(() -> ExceptionUtil.returnInvalidValueException());
+    }
+
+    public String getKoreanLabel() {
+        return koreanLabel;
+    }
+}
+
