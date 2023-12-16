7b2623dadc9bab2fee3cb49f4581a5d951da3246
inpink <inpink@kakao.com>
2023-12-16 16:58:27 +0900

    feat: 휴일인지 판단

--- src/main/java/oncall/domain/date/holiday/Holidays.java
+++ src/main/java/oncall/domain/date/holiday/Holidays.java
@@ -2,6 +2,9 @@ package oncall.domain.date.holiday;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
+import oncall.domain.date.DayOfWeek;
+import oncall.domain.date.Month;
 
 public enum Holidays {
 
@@ -15,4 +18,19 @@ public enum Holidays {
     }
 
 
+    public static boolean isHoliday(Month month, int dayNumber, DayOfWeek date) {
+        return isBasicHoliday(date)
+                || isLegalHoliday(month, dayNumber);
+    }
+
+    public static boolean isBasicHoliday(DayOfWeek date) {
+        return Stream.of(BasicHoliday.values())
+                .anyMatch(holiday -> holiday.getKoreanDescription().equalsIgnoreCase(date.getKoreanDescription()));
+    }
+
+    public static boolean isLegalHoliday(Month month, int dayNumber) {
+        return Stream.of(LegalHoliday.values())
+                .anyMatch(holiday -> holiday.getMonth() == month.getMonthNumber()
+                        && holiday.getDay() == dayNumber);
+    }
 }
