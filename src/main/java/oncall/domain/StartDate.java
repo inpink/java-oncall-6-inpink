a4b1169315c2069fe8de987d49c20a0a32ae9847
inpink <inpink@kakao.com>
2023-12-16 14:19:59 +0900

    feat: 정수가 아닌 월을 입력하면 잘못 입력한 것

--- src/main/java/oncall/domain/StartDate.java
+++ src/main/java/oncall/domain/StartDate.java
@@ -1,6 +1,19 @@
 package oncall.domain;
 
 
+import java.time.Month;
+
 public class StartDate {
 
+    private final Month month;
+    private final DayOfWeek day;
+
+    public StartDate(int month, String day) {
+        this.month = month;
+        this.day = day;
+    }
+
+    public static StartDate create(int month, String day) {
+
+    }
 }
