4f5ee5c6b1bd8f3721171a3d3748e109df305999
inpink <inpink@kakao.com>
2023-12-16 14:20:27 +0900

    feat: 월과 시작요일을 1개씩 입력하지 않으면 잘못된 입력

---
+++ src/main/java/oncall/constants/IntegerConstants.java
@@ -0,0 +1,21 @@
+package oncall.constants;
+
+public enum IntegerConstants {
+
+    MONTH_AND_DAY_COUNT(2);
+
+    private final int value;
+
+    IntegerConstants(final int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(getValue());
+    }
+}
